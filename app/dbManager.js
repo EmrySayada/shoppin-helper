db5c11587af8feb3deead2c35d992dadfc786d4f
EmrySayada <emrin276@outlook.com>
2025-05-14 21:33:22 +0300

    added all the logic

--- app/dbManager.js
+++ app/dbManager.js
@@ -1,24 +1,46 @@
-import { setDoc, doc, updateDoc, arrayUnion, collection, getDocs } from "firebase/firestore";
+import { setDoc, doc, updateDoc, arrayUnion, collection, getDocs, getDoc } from "firebase/firestore";
 import {db} from './firebase';
 
 export async function addItem(code, name, price, place){
-    const docRef = doc(db, 'items', code);
-    try{
-      await updateDoc(docRef, {
-          prices: arrayUnion(price),
-          cheapest: place 
-      })
-    }catch (FirebaseError){
-      await setDoc(docRef, {
-        code: code,
-        name: name,
+  var item = await getSingleItem(code);
+  const docRef = doc(db, 'items', code);
+  try{
+    await updateDoc(docRef, {
         prices: arrayUnion(price),
-        cheapest: place,
-        avgPrice: price
-      })
-    }
+        cheapest: getArrayMin(item.prices) > price ? place : item.place,
+        price: getArrayMin(item.prices) > price ? price : getArrayMin(item.prices)
+    })
+  }catch (FirebaseError){
+    await setDoc(docRef, {
+      code: code,
+      name: name,
+      prices: arrayUnion(price),
+      cheapest: place,
+      price: price
+    })
+  }
 }
 
+function getArrayMin(arr){
+  var min = arr[0];
+  for(let i = 0; i< arr.length; i++){
+    if(min > arr[i]){
+      min = arr[i];
+    }
+  }
+  return min;
+}
+
+async function getSingleItem(code){
+  const docRef = doc(db, "items", code);
+  const docSanp = await getDoc(docRef);
+
+  if(docSanp.exists()){
+    return docSanp.data();
+  }else{
+    console.log("skibidi");
+  }
+}
 
 export async function getItems(){
   const itemsArr = [];
--- app/page.js
+++ app/page.js
@@ -20,12 +20,12 @@ export default function Home() {
   return (
     <div className="h-screen w-[100%] flex flex-col item-center">
       <section className="flex w-[100%] h-[50%] items-center justify-center">
-        <input className=" w-[45%] h-[15%] rounded-[10px] border-[5px] border-[#03a9fc] pl-[10px] text-[18px]" placeholder="What item are you looking for?" onChange={async (val) => {setCurrItems(searchItem(val.target.value, items))}}/>
-        <Link href={'/newitem'} className="bg-[#03a9fc] ml-[10px] h-[15%] w-[5%] rounded-full flex items-center justify-center text-white text-[35px] hover:bg-white hover:text-[#03a9fc] hover:border-[#03a9fc] hover:border-[5px] transition-all">+</Link>
+        <input className=" w-[45%] h-[70px] rounded-[10px] border-[5px] border-[#03a9fc] pl-[10px] text-[18px]" placeholder="What item are you looking for?" onChange={async (val) => {setCurrItems(searchItem(val.target.value, items))}}/>
+        <Link href={'/newitem'} className="bg-[#03a9fc] ml-[10px] h-[70px] w-[70px] rounded-full flex items-center justify-center text-white text-[35px] hover:bg-white hover:text-[#03a9fc] hover:border-[#03a9fc] hover:border-[5px] transition-all">+</Link>
       </section>
       <section className="flex w-[100%] h-[50%] items-center justify-center flex-wrap">
           {currItems.map((item) => {
-            return <Item name={item.name} price={item.avgPrice} code={item.code} cheapest={item.cheapest} key={currItems.indexOf(item)}/>
+            return <Item name={item.name} price={item.price} code={item.code} cheapest={item.cheapest} key={currItems.indexOf(item)}/>
           })}
       </section>
     </div>
